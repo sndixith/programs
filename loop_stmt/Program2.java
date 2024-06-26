cfed18af5a88b6388fc286750d9dcba005f23188
sndixith <dixith.sn@gmail.com>
2024-06-26 19:45:43 +0530

    commit-4

---
+++ loop_stmt/Program2.java
@@ -0,0 +1,14 @@
+/*
+Write a program to find the factors for a given natural numbers.
+Test Case 1: 
+	Input : 9
+	Output : 1,3,9
+
+Test Case 2: 
+	Input : 15 
+	Output : 1,3,5,15
+*/
+
+public class Program2 {
+    
+}
