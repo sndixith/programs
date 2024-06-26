928e81dc1efc90be255954cbf805f04ada46a4b1
sndixith <dixith.sn@gmail.com>
2024-06-26 18:18:55 +0530

    commit_3

---
+++ loop_stmt/Program1.java
@@ -0,0 +1,17 @@
+/*
+Write a Java Program to print the Factorial of the given number.
+Test Case 1: 
+	Input : 3
+	Output : 6
+
+Test Case 2: 
+	Input : 5
+	Output : 120
+
+Test Case 3: 
+	Input : 9
+	Output : 362880
+*/
+class Program1{
+    
+}
