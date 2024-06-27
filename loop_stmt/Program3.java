cd2213bbad988e413dcd00234db04473ebee6401
sndixith <dixith.sn@gmail.com>
2024-06-27 18:47:06 +0530

    commit-5

---
+++ loop_stmt/Program3.java
@@ -0,0 +1,17 @@
+/*
+Write a program to check whether the entered 
+number is prime number or not.
+
+Test Case 1: 
+	Input : 7
+	Output : true
+
+Test Case 2: 
+	Input : 15
+	Output : false
+
+*/
+
+public class Program3 {
+    
+}
