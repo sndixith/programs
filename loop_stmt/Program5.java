5c318945cf2aa1b3f09349cea2ebe284fd2e0687
sndixith <dixith.sn@gmail.com>
2024-06-28 19:00:15 +0530

    commit-6

---
+++ loop_stmt/Program5.java
@@ -0,0 +1,14 @@
+/*
+Write a program to Calculate the LCM of 
+two given natural numbers.
+
+Test Case 1: 
+	Input : 15 
+	Input : 25
+	Output: 75
+
+Test Case 2: 
+	Input : 20
+	Input : 30
+	Output: 60
+*/
