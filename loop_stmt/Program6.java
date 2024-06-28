75229ad5711f88059ee93439317a271b6ae652ad
sndixith <dixith.sn@gmail.com>
2024-06-28 20:05:16 +0530

    commit-6

---
+++ loop_stmt/Program6.java
@@ -0,0 +1,29 @@
+/*
+Write a program to find the reverse of a given number 
+and check whether the given number is palindrome or not.
+
+Description:
+
+A palindrome is a word, sentence, verse, or even number 
+that reads the same backward or forward.
+
+Input : 12321
+Reverse of the given number: 12321
+The input and reverse number are same so it's called Palindrome
+
+Test Case 1: 
+	Input : 8767 
+	Output : false
+
+Test Case 2: 
+	Input : 985589
+	Output : true
+
+Test Case 3: 
+	Input : 65298
+	Output : false
+*/
+
+public class Program6 {
+    
+}
