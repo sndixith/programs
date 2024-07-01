89559006b77ac234d57b70d3575d14296b22a86b
sndixith <dixith.sn@gmail.com>
2024-07-01 19:51:41 +0530

    commit7

--- loop_stmt/Program6.java
+++ loop_stmt/Program6.java
@@ -23,7 +23,28 @@ Test Case 3:
 	Input : 65298
 	Output : false
 */
-
+import java.util.Scanner  ; 
 public class Program6 {
     
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in)  ; 
+		// read 1 num 
+		int num  = s.nextInt() ; 
+		int temp = num ; 
+		// logic to reverse 
+		// initialize rev to 0 
+		int reverse  = 0  ; 
+		while( num != 0 ){
+			int digit  = num % 10 ; 
+			reverse  = reverse * 10 + digit  ;  
+			num = num /  10  ; 
+		}
+		System.out.println(reverse);
+		// check for palindrome 
+		// compare original number with reversed number 
+		if( temp == reverse  )
+			System.out.println("it is palindrome");
+		else
+			System.out.println("it is not palindrome");
+	}
 }
