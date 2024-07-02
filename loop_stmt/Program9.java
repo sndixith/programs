b3097ce3682df5511bfe56eca41a0b04290936fd
sndixith <dixith.sn@gmail.com>
2024-07-02 19:45:43 +0530

    commit-8

---
+++ loop_stmt/Program9.java
@@ -0,0 +1,27 @@
+/*
+ * 
+You are developing a math learning application that includes a 
+feature to help students identify and understand special numbers.
+
+A special number is defined as a positive integer with m digits, 
+that equals the sum of its digits, each raised to the power of m.
+To determine if a number is special:
+Calculate the number of digits m.
+Compute the sum of each digit raised to the power of m.
+Check if this sum equals the original number.
+
+Explanation:
+
+Number: 153
+
+It has 3 digits (m=3)
+
+Sum of Digits Raised to the Power of m:
+=1^3+5^3+3^3
+=1+125+27=>153
+=153=153
+
+ */
+public class Program9 {
+    
+}
