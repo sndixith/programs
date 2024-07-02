679c3bfdb2e03d609276bdb63185442b996fa9fb
sndixith <dixith.sn@gmail.com>
2024-07-02 20:16:36 +0530

    commit-8

---
+++ loop_stmt/Program10.java
@@ -0,0 +1,42 @@
+/*
+ * You are developing a biology learning application that 
+ * introduces students to the concept of balance in numbers.
+As part of an interactive quiz feature, you want to create a 
+program that determines if a given number exhibits "Harmony."
+Concept:
+Harmonious Number: A positive integer N is classified as a 
+Harmonious Number if the sum of its first and last digits equals 
+the sum of its remaining digits.
+Non-Harmonious Number: If the sum of the first and last digits 
+does not equal the sum of the remaining digits, the number is 
+classified as Non-Harmonious.
+Example:
+For input number 2468:
+First digit and Last digit: 2,8
+Remaining digits: 4, 6
+Sum of first and last digits = 2 + 8 = 10
+Sum of remaining digits = 4 + 6 = 10
+Since the sum of first and last digits equals the sum of remaining
+digits, 2468 is a Harmonious Number.
+Test Case 1: 
+
+	Input  : 1235
+	Output : Harmony
+
+Test Case 2: 
+
+	Input  : 2468
+	Output : Harmony
+
+Test Case 3: 
+
+	Input  : 9474
+	Output : Non-Harmony
+
+
+
+ */
+
+public class Program10 {
+    
+}
