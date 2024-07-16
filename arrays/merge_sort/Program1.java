10c7298cd72605bac64d5d8bbd8cbc5b91bc37c4
sndixith <dixith.sn@gmail.com>
2024-07-16 20:18:29 +0530

    commit-14

---
+++ arrays/merge_sort/Program1.java
@@ -0,0 +1,16 @@
+/*
+ *  To Merge 2 sorted arrays 
+ * 
+ *  input: 
+ *          a1 : [ 2, 4 , 7] 
+ *          a2 : [ 3, 5, 6,8 ]
+ * 
+ *  output: 
+ *          a : [2,3,4,5,6,7,8]
+ */
+
+class Program1{
+    public static void main(String[] args) {
+        
+    }
+}
